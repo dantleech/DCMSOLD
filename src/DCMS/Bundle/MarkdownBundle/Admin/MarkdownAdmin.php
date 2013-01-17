c22379ba19d250515d2949e694741225c03361f5
dantleech <dan.t.leech@gmail.com>
2013-01-17 16:14:24 +0100

    Fixed added logo and translations

---
+++ src/DCMS/Bundle/MarkdownBundle/Admin/MarkdownAdmin.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace DCMS\Bundle\
+
+/**
+ * Description of MarkdownAdmin
+ *
+ * @author Daniel Leech <daniel@dantleech.com>
+ */
+class MarkdownAdmin
+{
+    //put your code here
+}
+
+?>
